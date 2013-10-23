77823a33696ea1995bff88a29ca0a4a337f4f3c7
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-10-22 23:12:30 -0500

    Created pmt() function

--- challenge_1.rb
+++ challenge_1.rb
@@ -13,8 +13,8 @@
 #   we usually have to do some research on how the task is done:
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
 
-# =========================================
-# Your code to define the method goes here.
-# =========================================
+def pmt(interest_rate,  number_of_payments, present_value)
+  monthly_payment = ((present_value * interest_rate) / (1 - (1 + interest_rate) ** -number_of_payments))
+  return "$ #{monthly.payment.round(2)}"
 
 puts "Your monthly payment will be #{pmt(0.01, 60, 30000)}."
